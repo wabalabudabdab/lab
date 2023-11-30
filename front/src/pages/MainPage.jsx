a4a14d0c4f629c7d1eafc1f1c22cff7ec8043e77
Anna Terekhova <a.terekhova@dataduck.com>
2023-11-30 14:09:00 +0300

    more

--- front/src/pages/MainPage.jsx
+++ front/src/pages/MainPage.jsx
@@ -103,7 +103,7 @@ export const MainPage = () => {
                                     />
                                     <div className="flex flex-col">
                                         <p className="text-sm leading-6 text-gray-700">{post.text}</p>
-                                        <button className="border-b-green-500 rounded bg-emerald-400 py-2 px-4 mt-4 text-white shadow-md max-w-xs ">
+                                        <button className="border-b-green-500 rounded bg-emerald-400 py-2 px-4 mt-4 ml-4 text-white shadow-md max-w-xs ">
                                                 <Link className="max-w-xs" to={`/${post._id}`}>Перейти в профиль</Link>
                                         </button>
                                     </div>

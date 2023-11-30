7731125dec97f87486e0d3b5076010caaa9919a1
Anna Terekhova <a.terekhova@dataduck.com>
2023-11-30 10:38:42 +0300

    more

--- front/src/pages/AddPostPage.jsx
+++ front/src/pages/AddPostPage.jsx
@@ -30,13 +30,13 @@ export const AddPostPage = () => {
 
     return (
         <>
-            <button className='text-white'>
+            <button className='text-black'>
                 <Link to={'/'}>
                     Назад
                 </Link>
             </button>
             <form
-                className='w-1/3 mx-auto py-10 bg-1c303f'
+                className='w-3/3 mx-auto py-10 bg-1c303f'
                 onSubmit={(e) => e.preventDefault()}
             >
                 <label className='text-gray-300 py-2 bg-gray-600 text-xs mt-2 flex items-center justify-center border-2 border-dotted cursor-pointer'>
